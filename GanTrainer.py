e57dcf138c07cde390e54e2a20fde6d761d740c9
pdkary <pdkary@gmail.com>
2021-08-12 01:10:03 -0400

    removed sigmoid from final discrimnator

--- GanTrainer.py
+++ GanTrainer.py
@@ -29,6 +29,7 @@ class GanTrainer(DCGAN):
 
     self.ones = np.ones((self.batch_size, 1), dtype=np.float32)
     self.zeros = np.zeros((self.batch_size, 1), dtype=np.float32)
+    self.nones = -self.ones
     
     print("Preparing Dataset".upper())
     self.images = DataHelper.load_data(self.data_path,self.img_shape,self.image_type,self.flip_lr,self.load_n_percent)
@@ -71,7 +72,7 @@ class GanTrainer(DCGAN):
     self.set_trainable(False,True)
     ##labels: {real: 1, fake: 0.5, noise: 0, homo: 0}
     ## disc outputs are [real, fake, noise, homogenous]
-    d_losses = self.DisModel.train_on_batch(training_data,[self.ones,self.ones/2.0,self.zeros,self.zeros])
+    d_losses = self.DisModel.train_on_batch(training_data,[self.ones,self.nones,self.zeros,self.zeros])
     label = self.DisModel.metrics_names.index('discriminator_base_accuracy')
     return d_losses[0],d_losses[label]
     
