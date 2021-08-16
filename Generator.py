177c7161d7d484f5f8eb8666f6293f4eb25a8e3c
pdkary <pdkary@gmail.com>
2021-08-15 21:30:44 -0400

    debuggin

--- Generator.py
+++ Generator.py
@@ -76,7 +76,6 @@ class Generator(GeneratorModelConfig,NoiseModelConfig,StyleModelConfig):
         out = self.gen_constant_input
         print(self.gen_layers[0])
         for layer_config in self.gen_layers:
-            print(layer_config)
             out = self.generator_block(out,style_model,noise_model,layer_config)
         out = Conv2D(self.img_shape[-1], layer_config.kernel_size, padding='same',activation='sigmoid')(out)
         
