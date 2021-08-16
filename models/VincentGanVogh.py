f4ce4bb9857404875aaf04b8ba33ceb851504714
pdkary <pdkary@gmail.com>
2021-08-16 19:59:35 -0400

    updated VGV py file

--- models/VincentGanVogh.py
+++ models/VincentGanVogh.py
@@ -16,31 +16,31 @@ style_model_config = StyleModelConfig(
 )
  
 noise_model_config = NoiseModelConfig(
-    noise_image_size = (256,256,4),
+    noise_image_size = (256,256,3),
     noise_kernel_size = 1,
-    gauss_factor = 0.75
+    gauss_factor = 1
 )
- 
+
 gen_model_config = GeneratorModelConfig(
-    img_shape = (256,256,4),
+    img_shape = (256,256,3),
     gen_constant_shape = (4,4,512),
     gen_layers = [
-        GenLayerConfig(512, 2, 5, LeakyReLU(0.05),      False, True, True),
-        GenLayerConfig(256, 2, 5, LeakyReLU(0.05),      True,  True, True),
-        GenLayerConfig(128, 2, 5, LeakyReLU(0.05),      True,  True, True),
-        GenLayerConfig(64,  2, 5, LeakyReLU(0.05),      True,  True, True),
-        GenLayerConfig(32,  2, 3, LeakyReLU(0.05),      True,  True, True),
-        GenLayerConfig(16,  2, 3, LeakyReLU(0.05),      True,  True, True),
-        GenLayerConfig(8,   3, 3, LeakyReLU(0.05),      True,  True, True),
-        GenLayerConfig(4,   1, 3, Activation('sigmoid'),False, True, False)],
+        GenLayerConfig(512, 3, 3, LeakyReLU,    {'alpha':0.05},          False, True, True ),
+        GenLayerConfig(256, 3, 3, LeakyReLU,    {'alpha':0.05},          True,  True, True ),
+        GenLayerConfig(128, 3, 3, LeakyReLU,    {'alpha':0.05},          True,  True, True ),
+        GenLayerConfig(64,  3, 3, LeakyReLU,    {'alpha':0.05},          True,  True, True ),
+        GenLayerConfig(32,  2, 3, LeakyReLU,    {'alpha':0.05},          True,  True, True ),
+        GenLayerConfig(16,  2, 3, LeakyReLU,    {'alpha':0.05},          True,  True, True ),
+        GenLayerConfig(8,   2, 3, LeakyReLU,    {'alpha':0.05},          True,  True, True ),
+        GenLayerConfig(3,   1, 3, Activation,   {'activation':'sigmoid'},False, True, False)],
     non_style_normalization_layer=BatchNormalization(momentum=0.8),
     gen_loss_function="binary_crossentropy",
-    gen_optimizer = Adam(learning_rate=0.002,beta_1=0.5)
+    gen_optimizer = Adam(learning_rate=5e-3,beta_1=0.0,beta_2=0.99,epsilon=1e-7)
 )
  
 ##VGG-19
 disc_model_config = DiscriminatorModelConfig(
-    img_shape = (256,256,4),
+    img_shape = (256,256,3),
     disc_conv_layers=[
         DiscConvLayerConfig(64,  2, 3, LeakyReLU(0.05), InstanceNormalization()),
         DiscConvLayerConfig(128, 2, 3, LeakyReLU(0.05), InstanceNormalization()),
@@ -54,18 +54,18 @@ disc_model_config = DiscriminatorModelConfig(
     minibatch=True,
     minibatch_size=4,
     disc_loss_function="binary_crossentropy",
-    disc_optimizer = Adam(learning_rate=0.002,beta_1=0.5)
+    disc_optimizer = Adam(learning_rate=5e-3,beta_1=0.0,beta_2=0.99,epsilon=1e-7)
 )
  
 gan_training_config = GanTrainingConfig(
     batch_size=4,
     preview_rows=4,
     preview_cols=6,
-    data_path='/content/drive/MyDrive/Colab/Mons',
+    data_path='/content/drive/MyDrive/Colab/Handwritten/English',
     image_type=".png",
-    model_name='/GANVogh_generator_model_',
-    flip_lr=True,
-    load_n_percent=100
+    model_name='/GANVogh_char_generator_model_',
+    flip_lr=False,
+    load_n_percent=10
 )
  
 VGV = GanTrainer(gen_model_config,noise_model_config,style_model_config,disc_model_config,gan_training_config)
