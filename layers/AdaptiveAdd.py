9fe32e64edf6a3a23aaa6dc9742cb35db6d0b464
pdkary <pdkary@gmail.com>
2021-08-19 01:17:12 -0400

    fixed bug in adaptiveadd

--- layers/AdaptiveAdd.py
+++ layers/AdaptiveAdd.py
@@ -12,8 +12,6 @@ class AdaptiveAdd(Layer):
 
     def call(self, inputs):
         conv_tensor,noise_tensor = inputs
-        print(conv_tensor.shape,noise_tensor.shape)
         weights = tf.nn.softmax(self.W, axis=-1)
         out = conv_tensor + weights*noise_tensor
-        print(out.shape)
-        return tf.reduce_sum(out, axis=-1) # (n_batch, n_feat) 
+        return out
