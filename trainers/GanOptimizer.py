a70c9e1d232e045ad6bbe600fd66f51b53cb4dac
pdkary <pdkary@gmail.com>
2021-08-26 01:04:07 -0400

    added image saving and tune_n_batches

--- trainers/GanOptimizer.py
+++ trainers/GanOptimizer.py
@@ -1,8 +1,8 @@
 
 
 
+import numpy as np
 import tensorflow as tf
-from tensorflow.python import training
 from models.GanInput import RealImageInput
 from typing import List
 from keras_tuner import BayesianOptimization
@@ -22,7 +22,8 @@ class GanOptimizer(GanTrainingConfig):
         self.generator = HyperGenerator("hyper_generator",True,4,24)
         self.image_source: RealImageInput = RealImageInput(data_config)
         self.image_source.load()
-        
+        self.model_output_path = data_config.data_path + "/models"
+
         self.gen_tuner = BayesianOptimization(
             self.generator,
             objective="accuracy",
@@ -33,12 +34,21 @@ class GanOptimizer(GanTrainingConfig):
             objective="accuracy",
             max_trials=5)
 
-    def tune(self):
+    def tune_on_batch(self,epoch):
         gen_input = self.generator.G.get_input()
         gen_images = self.generator.Gmodel(gen_input,training=False)
         real_images = self.image_source.get_batch()
         
-        self.gen_tuner.search(gen_input,real_images,epochs=2)
-        self.disc_tuner.search(real_images,tf.ones(shape=(self.batch_size)),epochs=2)
-        self.disc_tuner.search(gen_images,tf.zeros(shape=(self.batch_size)),epochs=2)
+        self.gen_tuner.search(gen_input,real_images,epochs=5)
+        self.disc_tuner.search(real_images,tf.ones(shape=(self.batch_size)),epochs=5)
+        self.disc_tuner.search(gen_images,tf.zeros(shape=(self.batch_size)),epochs=5)
         
+        preview_seed = self.generator.G.get_input(training=False)
+        generated_images = np.array(self.generator.Gmodel.predict(preview_seed))
+        self.image_source.save(epoch,generated_images)
+    
+    def tune_n_batches(self,n):
+        for i in range(n):
+            self.tune_on_batch(i)
+        filename = self.image_source.data_helper.model_name + "%d"%((i+1)*n)
+        self.generator.Gmodel.save(self.model_output_path + filename)
