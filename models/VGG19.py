7597523b1bc66f891e0a5d722f952b1a736fe065
pdkary <pdkary@gmail.com>
2021-08-29 17:40:38 -0400

    added optimizer to vgg19 constructor

--- models/VGG19.py
+++ models/VGG19.py
@@ -1,12 +1,13 @@
 from tensorflow.python.keras.losses import BinaryCrossentropy
 from config.DiscriminatorConfig import DiscriminatorModelConfig, DiscConvLayerConfig,DiscDenseLayerConfig, ActivationConfig, NormalizationConfig
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Optimizer
 
 def get_vgg19(input_channels:int,
               conv_activation:ActivationConfig,
               dense_activation:ActivationConfig,
               final_activation:ActivationConfig,
-              normalization:NormalizationConfig):
+              normalization:NormalizationConfig,
+              optimizer: Optimizer):
     return DiscriminatorModelConfig(
         img_shape = (256,256,input_channels),
         disc_conv_layers=[
@@ -22,6 +23,6 @@ def get_vgg19(input_channels:int,
             DiscDenseLayerConfig(1,    final_activation, 0.5)],
         minibatch=True,
         minibatch_size=32,
-        disc_optimizer = Adam(learning_rate=0.002,beta_1=0.0,beta_2=0.99,epsilon=1e-8),
+        disc_optimizer = optimizer,
         loss_function=BinaryCrossentropy())
     
