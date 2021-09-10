2a8cbd954e924dd0350dd73bd77e012c5098dc18
pdkary <pdkary@gmail.com>
2021-09-10 02:04:24 -0400

    updated VGG19

--- models/VGG19.py
+++ models/VGG19.py
@@ -9,22 +9,24 @@ def get_vgg19(input_channels:int,
               normalization:NormalizationConfig,
               optimizer: Optimizer,
               minibatch_size:int = 32,
-              dropout_rate:float = 0.5):
-    return DiscriminatorModelConfig(
+              dropout_rate:float = 0.5,
+              lite:bool = False):
+    d_c = lambda f,c: DiscConvLayerConfig(f,c,3,conv_activation,normalization)
+    d_d = lambda s : DiscDenseLayerConfig(s,dense_activation,dropout_rate)
+    d_out = DiscDenseLayerConfig(1,    final_activation, 0.0)
+    
+    D = DiscriminatorModelConfig(
         img_shape = (256,256,input_channels),
-        disc_conv_layers=[
-            DiscConvLayerConfig(64,  2, 3, conv_activation, normalization),
-            DiscConvLayerConfig(128, 2, 3, conv_activation, normalization),
-            DiscConvLayerConfig(256, 3, 3, conv_activation, normalization),
-            DiscConvLayerConfig(512, 3, 3, conv_activation, normalization),
-            DiscConvLayerConfig(512, 3, 3, conv_activation, normalization)],
-        disc_dense_layers=[
-            DiscDenseLayerConfig(4096, dense_activation, dropout_rate),
-            DiscDenseLayerConfig(4096, dense_activation, dropout_rate),
-            DiscDenseLayerConfig(1000, dense_activation, dropout_rate),
-            DiscDenseLayerConfig(1,    final_activation, 0.0)],
+        disc_conv_layers = [d_c(64,2),d_c(128,2),d_c(256,3),d_c(512,3),d_c(512,3)],
+        disc_dense_layers = [d_d(4096),d_d(4096),d_d(1000),d_out],
         minibatch=True,
         minibatch_size=minibatch_size,
         disc_optimizer = optimizer,
         loss_function=BinaryCrossentropy())
+    
+    if lite:
+        D.disc_conv_layers = D.disc_conv_layers[:-1] + 2*[D.disc_conv_layers[-1]]
+    return D
+    
+    
     
