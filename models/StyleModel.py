e8033c8cb05feb4884dd85b2befb8abfa5e634e1
pdkary <pdkary@gmail.com>
2021-09-15 15:46:45 -0400

    added downsampling to image style model

--- models/StyleModel.py
+++ models/StyleModel.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+
+from tensorflow.python.keras.layers.pooling import MaxPooling2D
 from config.CallableConfig import ActivationConfig
 from typing import Tuple
 from layers.GanInput import RealImageInput
@@ -41,9 +43,11 @@ class ImageStyleModel(StyleModelBase):
                  real_image_input: RealImageInput,
                  activation: ActivationConfig,
                  style_layers: int,
-                 style_layer_size: int):
+                 style_layer_size: int,
+                 downsample_factor: int = 1):
         self.image_source = real_image_input
         super().__init__(real_image_input.input_shape,activation,style_layers,style_layer_size)
+        self.model = MaxPooling2D((downsample_factor,downsample_factor))(self.model)
         self.model = Flatten()(self.model)
         for i in range(style_layers):
             self.model = Dense(style_layer_size, kernel_regularizer=L2(), kernel_initializer='he_normal')(self.model)
