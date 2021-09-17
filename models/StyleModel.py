2b9fe51de0879183d8641d62605e7ac8f2c04509
pdkary <pdkary@gmail.com>
2021-09-17 00:35:56 -0400

    removed gen/disc config files

--- models/StyleModel.py
+++ models/StyleModel.py
@@ -5,49 +5,51 @@ import tensorflow as tf
 from config.GanConfig import ActivationConfig, RegularizationConfig
 from layers.GanInput import RealImageInput
 from tensorflow.keras.layers import Dense, Flatten, Input
-from tensorflow.python.keras.layers.convolutional import Conv2D
-from tensorflow.python.keras.layers.pooling import MaxPooling2D
+from tensorflow.python.keras.layers import Conv2D
+from tensorflow.python.keras.layers import MaxPooling2D
 
 
 class StyleModelBase(ABC):
-    def __init__(self, 
-                 input_shape: Tuple, 
-                 style_layers: int, 
-                 style_layer_size: int, 
+    def __init__(self,
+                 input_shape: Tuple,
+                 style_layers: int,
+                 style_layer_size: int,
                  activation: ActivationConfig):
         self.input_shape = input_shape
         self.style_layers = style_layers
         self.style_layer_size = style_layer_size
         self.activation = activation
-        self.input = Input(shape=input_shape,name="style_model_input")
-    
+        self.input = Input(shape=input_shape, name="style_model_input")
+
     @abstractmethod
-    def get_training_batch(self,batch_size:int):
+    def get_training_batch(self, batch_size: int):
         pass
-    
+
     @abstractmethod
-    def get_validation_batch(self,batch_size:int):
+    def get_validation_batch(self, batch_size: int):
         pass
-        
+
+
 class LatentStyleModel(StyleModelBase):
-    def __init__(self, 
-                 input_shape: Tuple, 
-                 style_layers: int, 
-                 style_layer_size: int, 
+    def __init__(self,
+                 input_shape: Tuple,
+                 style_layers: int,
+                 style_layer_size: int,
                  activation: ActivationConfig):
         super().__init__(input_shape, style_layers, style_layer_size, activation)
-        
+
         model = self.input
         for i in range(self.style_layers):
             model = Dense(self.style_layer_size)(model)
             model = self.activation.get()(model)
         self.model = model
-    
-    def get_training_batch(self,batch_size:int):
-        return tf.random.normal(shape = (batch_size,self.input_shape),dtype=tf.float32)
-    
+
+    def get_training_batch(self, batch_size: int):
+        return tf.random.normal(shape=(batch_size, self.input_shape), dtype=tf.float32)
+
     def get_validation_batch(self, batch_size: int):
         return self.get_training_batch(batch_size)
+
 
 class ImageStyleModel(StyleModelBase):
     def __init__(self,
@@ -63,23 +65,24 @@ class ImageStyleModel(StyleModelBase):
                  kernel_initializer: str = "glorot_uniform",
                  downsample_factor: int = 1):
         self.image_source = real_image_input
-        self.downsample_factor = (downsample_factor,downsample_factor)
-        super().__init__(real_image_input.input_shape,style_layers,style_layer_size,activation)
-        
+        self.downsample_factor = (downsample_factor, downsample_factor)
+        super().__init__(real_image_input.input_shape,
+                         style_layers, style_layer_size, activation)
+
         model = MaxPooling2D(self.downsample_factor)(self.input)
         for i in range(convolutions):
-            model = Conv2D(filters,kernel_size,padding="same")(model)
+            model = Conv2D(filters, kernel_size, padding="same")(model)
             model = conv_activation.get()
         model = Flatten()(model) if self.style_layers > 0 else model
         for i in range(self.style_layers):
-            model = Dense(self.style_layer_size, 
-                          kernel_regularizer=kernel_regularizer.get(), 
+            model = Dense(self.style_layer_size,
+                          kernel_regularizer=kernel_regularizer.get(),
                           kernel_initializer=kernel_initializer)(model)
             model = self.activation.get()(model)
         self.model = model
-        
-    def get_training_batch(self,batch_size):
+
+    def get_training_batch(self, batch_size):
         return self.image_source.get_training_batch(batch_size)
-        
-    def get_validation_batch(self,batch_size):
+
+    def get_validation_batch(self, batch_size):
         return self.image_source.get_validation_batch(batch_size)
