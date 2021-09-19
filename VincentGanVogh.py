336fe8c64ccba8ec272e5fa59e2f1d37b7b59701
pdkary <pdkary@gmail.com>
2021-09-19 16:47:13 -0400

    updated vgv

--- VincentGanVogh.py
+++ VincentGanVogh.py
@@ -1,82 +1,128 @@
-from models.GanInput import GenConstantInput, GenLatentSpaceInput
+import numpy as np
+from tensorflow.keras.layers import Activation, BatchNormalization, LeakyReLU
+from tensorflow.keras.losses import binary_crossentropy,categorical_crossentropy, kl_divergence, mean_squared_error
+from tensorflow.keras.metrics import Accuracy, Mean, MeanSquaredError
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import BinaryCrossentropy
-from third_party_layers.InstanceNormalization import InstanceNormalization
-from tensorflow.keras.layers import BatchNormalization, LeakyReLU, Activation
-from config.TrainingConfig import DataConfig, GanTrainingConfig
-from trainers.GenTapeTrainer import GenTapeTrainer
-from config.CallableConfig import *
-from config.GeneratorConfig import *
- 
-from models.VGG19 import get_vgg19
+from tensorflow.keras.regularizers import L2
 
+from config.GanConfig import GenLayerConfig
+from config.TrainingConfig import DataConfig, GanTrainingConfig
+from layers.CallableConfig import ActivationConfig, NormalizationConfig,RegularizationConfig
+from layers.GanInput import GenConstantInput, GenLatentSpaceInput, RealImageInput
+from models.Discriminator import Discriminator
+from models.Generator import Generator
+from models.NoiseModel import ConstantNoiseModel, LatentNoiseModel
+from models.StyleModel import ImageStyleModel, LatentStyleModel
+from models.VGG19 import get_vgg19
+from third_party_layers.InstanceNormalization import InstanceNormalization
+from trainers.CombinedTrainer import CombinedTrainer
+from trainers.GenTapeTrainer import GenTapeTrainer
+
+# from google.colab import drive
+# drive.mount('/content/drive')
+ 
 ##activations
-leakyRELU_style = ActivationConfig(LeakyReLU,dict(alpha=0.1))
-leakyRELU_conv = ActivationConfig(LeakyReLU,dict(alpha=0.08))
-sigmoid = ActivationConfig(Activation,dict(activation="sigmoid"))
-linear = ActivationConfig(Activation,dict(activation="linear"))
+gen_lr_dense = ActivationConfig(LeakyReLU,"gen_lr_dense",dict(alpha=0.1))
+gen_lr_conv = ActivationConfig(LeakyReLU,"gen_lr_conv",dict(alpha=0.08))
+
+disc_lr_dense = ActivationConfig(LeakyReLU,"disc_lr_dense",dict(alpha=0.1))
+disc_lr_conv = ActivationConfig(LeakyReLU,"disc_lr_conv",dict(alpha=0.08))
+
+sigmoid = ActivationConfig(Activation,"sigmoid",dict(activation="sigmoid"))
+softmax = ActivationConfig(Activation,"softmax",dict(activation="softmax"))
+tanh = ActivationConfig(Activation,"tanh",dict(activation="tanh"))
+relu = ActivationConfig(Activation,"relu",dict(activation="relu"))
+linear = ActivationConfig(Activation,"linear",dict(activation="linear"))
+
 ##normalizations
 instance_norm = NormalizationConfig(InstanceNormalization)
-batch_norm = NormalizationConfig(BatchNormalization,dict(momentum=0.5))
-##optimizers
-adam = CallableConfig(Adam,dict(learning_rate=0.002))
+batch_norm = NormalizationConfig(BatchNormalization,dict(momentum=0.8))
+##regularizers
+l2 = RegularizationConfig(L2)
+##desired image shape
 img_shape = (256,256,3)
- 
-gen_model_config = GeneratorModelConfig(
-    img_shape = img_shape,
-    input_model = GenConstantInput((4,4,512)),
-    # input_model = GenLatentSpaceInput(100,(4,4,512),512,2,leakyRELU_style),
-    
-    style_model_config = StyleModelConfig(
-        style_latent_size = 100,
-        style_layer_size = 512,
-        style_layers = 4,
-        style_activation = leakyRELU_style),
-    
-    noise_model_config = NoiseModelConfig(
-        noise_image_size = img_shape,
-        kernel_size = 1,
-        max_std_dev = 1),
- 
-    gen_layers = [
-        GenLayerConfig(512,           3, 3, leakyRELU_conv, style=True, transpose=True, strides=2, noise=True),
-        GenLayerConfig(512,           3, 3, leakyRELU_conv, style=True, transpose=True, strides=2, noise=True),
-        GenLayerConfig(256,           3, 3, leakyRELU_conv, style=True, transpose=True, strides=2, noise=True),
-        GenLayerConfig(128,           3, 3, leakyRELU_conv, style=True, transpose=True, strides=2, noise=True),
-        GenLayerConfig(64,            2, 3, leakyRELU_conv, style=True, upsampling=True, noise=True),
-        GenLayerConfig(32,            2, 3, leakyRELU_conv, style=True, upsampling=True, noise=True),
-        GenLayerConfig(img_shape[-1], 1, 1, sigmoid,        style=True)],
-    normalization = instance_norm,
-    gen_optimizer = adam.get(),
-    loss_function= BinaryCrossentropy()
-)
- 
-disc_model_config = get_vgg19(img_shape[-1],leakyRELU_conv,leakyRELU_style,sigmoid,instance_norm,adam.get(),64,0.4)
- 
-gan_training_config = GanTrainingConfig(
-    plot=True,
-    #[real_image_label, not_image_label]
-    disc_labels=[1.0,0.0],
-    #desired label
-    gen_label=0.5,
-    gen_batch_size=16,
-    disc_batch_size=4,
-    preview_rows=4,
-    preview_cols=6,
-    preview_margin=16
-)
- 
+
+def map_to_range(input_arr,new_max,new_min):
+  img_max = float(np.max(input_arr))
+  img_min = float(np.min(input_arr))
+  old_range = float(img_max - img_min + 1e-6)
+  new_range = (new_max - new_min)
+  return new_range*(input_arr - img_min)/old_range + float(new_min)
+
+#data input
 data_config = DataConfig(
     data_path='test_images',   
     image_type=".png",
     image_shape=img_shape,
     model_name='/simplegan_generator_model_',
     flip_lr=False,
-    load_n_percent=10
+    load_n_percent=10,
+    load_scale_function = lambda x: map_to_range(x,1.0,-1.0),
+    save_scale_function = lambda x: map_to_range(x,255.0,0.0)
 )
 
-VGV = GenTapeTrainer(gen_model_config,disc_model_config,gan_training_config,[data_config])
- 
+image_source = RealImageInput(data_config)
+
+##style models
+# style_model = ImageStyleModel(image_source,32,2,3,3,100,gen_lr_dense,gen_lr_conv,l2,downsample_factor=4)
+# style_model = LatentStyleModel(100,3,512,gen_lr_dense)
+style_model = None
+
+##noise model
+# noise_model = LatentNoiseModel(img_shape,gen_lr_conv,l2)
+noise_model = None
+
+##input models
+# input_model = GenConstantInput((4,4,512))
+input_model = GenLatentSpaceInput(100,(2,2,1024),512,3,gen_lr_dense)
+
+## layer shorthands 
+gl = lambda f,c: GenLayerConfig(f,c,3,gen_lr_conv,upsampling=True,style=True,noise=True)
+glt = lambda f,c: GenLayerConfig(f,c,3,gen_lr_conv,upsampling=True,transpose=True,style=True,noise=True)
+output_layer = GenLayerConfig(img_shape[-1],1,1,tanh,style=True)
+
+#Generator model
+generator = Generator(
+    img_shape = img_shape,
+    input_model = input_model,
+    gen_layers = [gl(512,3),gl(512,3),gl(256,3),gl(256,3),gl(128,3),gl(128,3),gl(64,3),output_layer],
+    gen_optimizer = Adam(learning_rate=2e-3,beta_1=0.0),
+    loss_function = binary_crossentropy,
+    normalization = batch_norm
+)
+
+#Discriminator Model
+discriminator = get_vgg19(
+    input_channels = img_shape[-1],
+    conv_activation = disc_lr_conv,
+    dense_activation = disc_lr_dense,
+    final_activation = softmax,
+    normalization = batch_norm,
+    optimizer = Adam(learning_rate=2e-3,beta_1=0.0),
+    loss_function = binary_crossentropy,
+    lite = True
+)
+
+#Training config
+gan_training_config = GanTrainingConfig(
+    plot=True,
+    #[real_image_label, not_image_label]
+    disc_labels=[1.0,0.0],
+    #desired label
+    gen_label=1.0,
+    batch_size=4,
+    disc_batches_per_epoch = 1,
+    gen_batches_per_epoch = 1,
+    metrics = [Accuracy,MeanSquaredError],
+    preview_rows=4,
+    preview_cols=6,
+    preview_margin=16
+)
+
+#Trainer
+VGV = GenTapeTrainer(generator,discriminator,gan_training_config,[image_source])
+
+
 #TRAINING
 ERAS = 1
 EPOCHS = 1
