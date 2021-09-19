7e041bf3583d148f532724626e1bb4c6058c6f66
pdkary <pdkary@gmail.com>
2021-09-19 18:33:44 -0400

    debuggin

--- layers/AdaptiveInstanceNormalization.py
+++ layers/AdaptiveInstanceNormalization.py
@@ -16,10 +16,16 @@ class AdaptiveInstanceNormalization(Layer):
     
 
 def adain(content_features,style_features):
+    print("content_features: ",content_features.shape)
+    print("style_features: ",style_features.shape)
     content_mean = K.mean(content_features,axis=[1,2],keepdims=True)
+    print("content_mean: ",content_mean.shape)
     content_std = K.std(content_features,axis=[1,2],keepdims=True) + 1e-7
+    print("content_std: ",content_std.shape)
     style_mean = K.mean(style_features,axis=[1,2],keepdims=True)
+    print("style_mean: ",style_mean.shape)
     style_std = K.std(style_features,axis=[1,2],keepdims=True) + 1e-7
+    print("style_std: ",style_std.shape)
     normed = (content_features - content_mean)/content_std
     return style_std*normed + style_mean
         
