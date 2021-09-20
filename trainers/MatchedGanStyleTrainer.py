c214ad99ee433b2cf88f3597efba61df46499310
pdkary <pdkary@gmail.com>
2021-09-20 00:00:31 -0400

    updated matchganstyle train disc function

--- trainers/MatchedGanStyleTrainer.py
+++ trainers/MatchedGanStyleTrainer.py
@@ -35,31 +35,32 @@ class MatchedGanStyleTrainer(AbstractTrainer):
         generated_images = np.array(self.generator.predict(preview_seed)[0])
         self.image_sources[0].save(epoch, generated_images, self.preview_rows, self.preview_cols, self.preview_margin)
         
-    def get_style_loss(self,source_style,desired_style):
+    def get_deep_style_loss(self,source_style,desired_style):
         src_2_dest = list(zip(source_style,desired_style))
         ada_outs = [adain(s,d) for s,d in src_2_dest]
         src_2_ada = list(zip(source_style,ada_outs))
         return [tf.losses.mean_squared_error(s,a) for s,a in src_2_ada]
+    
+    def get_style_loss(self,content_img,style_img):
+        ada_content = adain(content_img,style_img)
+        return tf.losses.mean_squared_error(content_img,ada_content)
         
     def train_generator(self,source_input, gen_input):
         with tf.GradientTape() as gen_tape:
             gen_out = self.generator(gen_input,training=True)
-            gen_images,gen_style = gen_out[0],gen_out[1:]
+            gen_images,gen_deep_layers = gen_out[0],gen_out[1:]
             
-            disc_gen_out = self.discriminator(gen_images, training=False)
-            disc_gen_content,disc_gen_style = disc_gen_out[0],disc_gen_out[1:]
+            disc_gen_out = self.discriminator(gen_images, training=False)[0]
+            disc_real_deep_layers = self.discriminator(source_input, training=False)[1]
             
-            disc_real_out = self.discriminator(source_input, training=False)
-            disc_real_content,disc_real_style = disc_real_out[0],disc_real_out[1:]
-            
-            content_loss = self.G.loss_function(self.gen_label, disc_gen_content)
-            
-            style_losses = self.get_style_loss(gen_style,reversed(disc_real_style))
-            g_loss = [content_loss + np.sum(style_losses),*style_losses]
+            style_loss = self.get_style_loss(gen_images,source_input)
+            deep_style_losses = self.get_deep_style_loss(gen_deep_layers,reversed(disc_real_deep_layers))
+            content_loss = self.G.loss_function(self.gen_label, disc_gen_out)
+            g_loss = [content_loss + style_loss,*deep_style_losses]
             out = [g_loss[0]]
             
             for metric in self.gen_metrics:
-                metric.update_state(self.gen_label,disc_gen_content)
+                metric.update_state(self.gen_label,disc_gen_out)
                 out.append(metric.result())
             
             gradients_of_generator = gen_tape.gradient(g_loss, self.generator.trainable_variables)
