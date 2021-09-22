400d9eb9daf0b92fe8f5c9b58d8bb2ca9a2e13e8
Parker Kary <pdkary@gmail.com>
2021-09-22 11:15:13 -0400

    added output style loss

--- trainers/MatchedGanStyleTrainer.py
+++ trainers/MatchedGanStyleTrainer.py
@@ -61,10 +61,12 @@ class MatchedGanStyleTrainer(AbstractTrainer):
             disc_out = self.discriminator(gen_images, training=False)
             disc_results,disc_deep_layers = disc_out[0],disc_out[1:]
             
+            output_style_loss = self.get_style_loss(gen_images,source_input)
             deep_style_losses = self.get_deep_style_loss(gen_deep_layers,disc_deep_layers)
             content_loss = self.G.loss_function(self.gen_label, disc_results)
-            g_loss = [content_loss,*deep_style_losses]
-            out = [content_loss,np.sum(deep_style_losses)]
+            total_loss = content_loss + output_style_loss
+            g_loss = [total_loss,*deep_style_losses]
+            out = [total_loss,np.sum(deep_style_losses)]
             
             for metric in self.G.metrics:
                 metric.update_state(self.gen_label,disc_results)
