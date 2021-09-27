2641cadc991893cc0bbd5ea2d04bb5a0db486717
pdkary <pdkary@gmail.com>
2021-09-27 18:51:40 -0400

    updated noise model to match style model structure

--- models/Generator.py
+++ models/Generator.py
@@ -1,3 +1,5 @@
+from layers.AdaptiveAdd import AdaptiveAdd
+from tensorflow.python.keras.layers.convolutional import Cropping2D
 from helpers.DataHelper import shape_to_key
 from typing import Tuple, List
 
@@ -51,6 +53,7 @@ class Generator():
             self.style_out = self.style_model.model(self.style_model.input)
             self.input.append(self.style_model.input)
         if self.noise_model is not None:
+            self.noise_out = self.noise_model.model(self.noise_model.input)
             self.input.append(self.noise_model.input)
     
     def get_training_input(self,batch_size):
@@ -91,6 +94,14 @@ class Generator():
         self.tracked_layers.append([beta,gamma])
         return beta,gamma
 
+    def get_noise(self,filters,h_w):
+        n_size = self.noise_model.noise_image_size[1]
+        noise = Cropping2D((n_size - h_w)//2)(self.noise_out)
+        noise = Conv2D(filters,self.noise_model.kernel_size,padding="same",
+                        kernel_regularizer=self.kernel_regularizer.get(),
+                        kernel_initializer=self.kernel_initializer,use_bias=False)(noise)
+        return noise
+
     def generator_block(self,input_tensor,config: GenLayerConfig):
         out = input_tensor
         out = UpSampling2D(interpolation='bilinear')(out) if config.upsampling else out
@@ -105,7 +116,8 @@ class Generator():
                              kernel_initializer = self.kernel_initializer, use_bias=False)(out)
             
             if self.noise_model is not None and config.noise:
-                out = self.noise_model.add(out)
+                noise = self.get_noise(config.filters,out.shape[1])
+                return AdaptiveAdd()([out,noise])
             
             if self.style_model is not None and config.style:
                 beta,gamma = self.get_beta_gamma(config.filters,out.shape,i)
--- models/NoiseModel.py
+++ models/NoiseModel.py
@@ -1,42 +1,27 @@
 from abc import ABC, abstractmethod
-from layers.GanInput import RealImageInput
 from typing import Tuple
 
 import numpy as np
 import tensorflow as tf
-from layers.CallableConfig import ActivationConfig, NoneCallable, RegularizationConfig
-from layers.AdaptiveAdd import AdaptiveAdd
-from tensorflow.keras.layers import Conv2D, Cropping2D, Input
+from layers.CallableConfig import ActivationConfig
+from layers.GanInput import RealImageInput
+from tensorflow.python.keras.models import Model
 
 
 class NoiseModelBase(ABC):
     def __init__(self,
                  noise_image_size: Tuple[int, int, int],
                  activation: ActivationConfig,
-                 kernel_regularizer: RegularizationConfig = NoneCallable,
-                 kernel_initializer: str = "glorot_normal",
                  kernel_size: int = 1,
                  max_std_dev: float = 1.0):
         self.noise_image_size = noise_image_size
         self.activation = activation
-        self.kernel_regularizer = kernel_regularizer
-        self.kernel_initializer = kernel_initializer
         self.kernel_size = kernel_size
         self.max_std_dev = max_std_dev
 
         self.input = Input(shape=self.noise_image_size,name="noise_model_input")
-        self.model = self.activation.get()(self.input)
-
-    def add(self, input_tensor):
-        n_size = self.model.shape[1]
-        i_size = input_tensor.shape[1]
-        noise = Cropping2D((n_size-i_size)//2)(self.model)
-        noise = Conv2D(input_tensor.shape[-1],
-                       self.kernel_size,
-                       padding='same',
-                       kernel_regularizer=self.kernel_regularizer.get(),
-                       kernel_initializer=self.kernel_initializer)(noise)
-        return AdaptiveAdd()([input_tensor, noise])
+        model = self.activation.get()(self.input)
+        self.model = Model(inputs=self.input,outputs=model)
 
     @abstractmethod
     def get_training_batch(self, batch_size: int):
@@ -51,11 +36,9 @@ class LatentNoiseModel(NoiseModelBase):
     def __init__(self, 
                  noise_image_size: Tuple[int, int, int], 
                  activation: ActivationConfig, 
-                 kernel_regularizer: RegularizationConfig = NoneCallable, 
-                 kernel_initializer: str = "glorot_uniform", 
                  kernel_size: int = 1, 
                  max_std_dev: float = 1.0):
-        super().__init__(noise_image_size, activation, kernel_regularizer,kernel_initializer, kernel_size, max_std_dev)
+        super().__init__(noise_image_size, activation, kernel_size, max_std_dev)
         
     def get_training_batch(self, batch_size: int):
         noise_batch = np.full((batch_size, *self.noise_image_size), 0.0, dtype=np.float32)
@@ -71,11 +54,9 @@ class ConstantNoiseModel(NoiseModelBase):
     def __init__(self, 
                  noise_image_size: Tuple[int, int, int], 
                  activation: ActivationConfig, 
-                 kernel_regularizer: RegularizationConfig, 
-                 kernel_initializer: str = "glorot_uniform", 
                  kernel_size: int = 1, 
                  max_std_dev: float = 1.0):
-        super().__init__(noise_image_size, activation, kernel_regularizer,kernel_initializer, kernel_size, max_std_dev)
+        super().__init__(noise_image_size, activation, kernel_size, max_std_dev)
         self.constant = tf.random.normal(shape=self.noise_image_size, stddev=self.max_std_dev)
 
     def get_training_batch(self, batch_size: int):
@@ -93,15 +74,14 @@ class ImageNoiseModel(NoiseModelBase):
     def __init__(self, 
                  image_source: RealImageInput, 
                  activation: ActivationConfig, 
-                 kernel_regularizer: RegularizationConfig, 
-                 kernel_initializer: str = "glorot_uniform", 
                  kernel_size: int = 1, 
                  max_std_dev: float = 1.0):
         self.image_source = image_source
-        super().__init__(image_source.image_shape, activation, kernel_regularizer, kernel_initializer, kernel_size, max_std_dev)
+        super().__init__(image_source.image_shape, activation, kernel_size, max_std_dev)
         
     def get_training_batch(self, batch_size):
         return self.image_source.get_training_batch(batch_size)
 
     def get_validation_batch(self, batch_size):
         return self.image_source.get_validation_batch(batch_size)
+
