52db599c8240fd2f2b27b2ecdec2473010e6d8a6
pdkary <pdkary@gmail.com>
2021-10-17 23:58:16 -0400

    added final activation to disc

--- layers/AdaptiveInstanceNormalization.py
+++ layers/AdaptiveInstanceNormalization.py
@@ -12,26 +12,40 @@ class AdaptiveInstanceNormalization(Layer):
         c_shape,s_shape = input_shape
         mat_shape = (c_shape[-1],s_shape[-1])
 
-        self.B = self.add_weight(
+        self.B_weight = self.add_weight(
             shape=mat_shape,
             dtype=tf.float32,
             initializer='ones',
             trainable=True,
-            name=self.layer_name + "_beta"
+            name=self.layer_name + "_Bw"
         )
-        self.G = self.add_weight(
-            shape=mat_shape,
+        self.B_bias = self.add_weight(
+            shape=c_shape[-1],
             dtype=tf.float32,
             initializer='zeros',
             trainable=True,
-            name=self.layer_name + "_gamma"
+            name=self.layer_name + "_Bb"
+        )
+        self.G_weight = self.add_weight(
+            shape=mat_shape,
+            dtype=tf.float32,
+            initializer='ones',
+            trainable=True,
+            name=self.layer_name + "_Gw"
+        )
+        self.G_bias = self.add_weight(
+            shape=c_shape[-1],
+            dtype=tf.float32,
+            initializer='zeros',
+            trainable=True,
+            name=self.layer_name + "_Gb"
         )
         
     def call(self, input_tensor):
         c_input,s_input = input_tensor
-        beta = tf.transpose(tf.linalg.matmul(self.B,s_input,transpose_b=True))
+        beta = tf.transpose(tf.linalg.matmul(self.B_weight,s_input,transpose_b=True)) + self.B_bias
         beta = K.reshape(beta,(-1,1,1,beta.shape[-1]))
-        gamma = tf.transpose(tf.linalg.matmul(self.G,s_input,transpose_b=True))
+        gamma = tf.transpose(tf.linalg.matmul(self.G_weight,s_input,transpose_b=True)) + self.G_bias
         gamma = K.reshape(gamma,(-1,1,1,gamma.shape[-1]))
         mean = K.mean(c_input, axis = [1,2], keepdims = True)
         std = K.std(c_input, axis = [1,2], keepdims = True) + 1e-7
