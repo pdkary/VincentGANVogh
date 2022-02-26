f36532b60d7dba0eba1b19282d825dbe2645754f
pdkary <pdkary@gmail.com>
2022-02-26 14:06:49 -0500

    added noise to unet params

--- models/UNET.py
+++ models/UNET.py
@@ -22,7 +22,7 @@ def generate_UNET(input: GanInput,F: int, C: int, depth = 5, up_noise = 0.1, dow
         conv_down_layers.append(down_layer((2**i)*F,C,3,concat_with=key,n=down_noise))
         conv_up_layers = [up_layer(F,C,3,concat_with=key,n=up_noise)] + conv_up_layers
     
-    conv_down_layers.append(down_layer((2**(depth - 1))*F,C,3),n=down_noise)
+    conv_down_layers.append(down_layer((2**(depth - 1))*F,C,3,n=down_noise))
     conv_down_layers.extend(conv_up_layers)
     conv_down_layers.append(up_layer(input.input_shape[-1],1,3,act=sigmoid,u=0))
     return Generator(
