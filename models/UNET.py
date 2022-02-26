a47bfe1883aad72b81dd0bcb3a027be8310a6de9
pdkary <pdkary@gmail.com>
2022-02-25 20:24:29 -0500

    lets normalize shall we

--- models/UNET.py
+++ models/UNET.py
@@ -1,18 +1,20 @@
 from config.CallableConfig import NoneCallable
-from config.GanConfig import DiscConvLayerConfig, GenLayerConfig, SimpleActivations
+from config.GanConfig import DiscConvLayerConfig, GenLayerConfig, SimpleActivations, SimpleNormalizations
 from inputs.GanInput import GanInput
 from models.Generator import Generator
 
 conv_lr = SimpleActivations.leakyRelu_p08.value
 sigmoid = SimpleActivations.sigmoid.value
 
-def up_layer(f,c,k,act=conv_lr,u=True,t=True,n=0.1,id="",norm=NoneCallable,concat_with=""):
+i_norm = SimpleNormalizations.instance_norm.value
+
+def up_layer(f,c,k,act=conv_lr,u=True,t=True,n=0.1,id="",norm=i_norm,concat_with=""):
   return GenLayerConfig(f,c,k,act,upsampling=u,transpose=t,noise=n,track_id=id,normalization=norm,concat_with=concat_with)
 
-def down_layer(f,c,k,act=conv_lr,d=True,t=False,dr=0.0,n=0.0,id="",norm=NoneCallable,concat_with=""):
+def down_layer(f,c,k,act=conv_lr,d=True,t=False,dr=0.0,n=0.0,id="",norm=i_norm,concat_with=""):
   return DiscConvLayerConfig(f,c,k,act,downsampling=d,transpose=t,dropout_rate=dr,track_id=id,normalization=norm,noise=n,concat_with=concat_with)
 
-def generate_UNET(input: GanInput,F: int, C):
+def generate_UNET(input: GanInput,F: int, C: int):
     return Generator(
         gan_input = input,
         conv_layers = [
