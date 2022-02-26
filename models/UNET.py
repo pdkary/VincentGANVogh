17ac532d7dded2304a515b5d8bb40cdf5180b9ed
pdkary <pdkary@gmail.com>
2022-02-25 20:15:25 -0500

    wait a minute thats not unet

--- models/UNET.py
+++ models/UNET.py
@@ -12,19 +12,19 @@ def up_layer(f,c,k,act=conv_lr,u=True,t=True,n=0.1,id="",norm=NoneCallable,conca
 def down_layer(f,c,k,act=conv_lr,d=True,t=False,dr=0.0,n=0.0,id="",norm=NoneCallable,concat_with=""):
   return DiscConvLayerConfig(f,c,k,act,downsampling=d,transpose=t,dropout_rate=dr,track_id=id,normalization=norm,noise=n,concat_with=concat_with)
 
-def generate_UNET(input: GanInput):
+def generate_UNET(input: GanInput,F: int, C):
     return Generator(
         gan_input = input,
         conv_layers = [
-            down_layer(3,1,3,concat_with="0"),
-            down_layer(3,2,3,concat_with="1"),
-            down_layer(3,4,3,concat_with="2"),
-            down_layer(3,8,3,concat_with="3"),
-            down_layer(3,16,3),
-            up_layer(3,8,3,concat_with="3"),
-            up_layer(3,4,3,concat_with="2"),
-            up_layer(3,2,3,concat_with="1"),
-            up_layer(3,1,3,concat_with="0"),
+            down_layer(  F, C,3,concat_with="0"),
+            down_layer(2*F, C,3,concat_with="1"),
+            down_layer(4*F, C,3,concat_with="2"),
+            down_layer(8*F, C,3,concat_with="3"),
+            down_layer(16*F, C,3),
+            up_layer(8*F,C,3,concat_with="3"),
+            up_layer(4*F,C,3,concat_with="2"),
+            up_layer(2*F,C,3,concat_with="1"),
+            up_layer(  F,C,3,concat_with="0"),
             up_layer(input.input_shape[-1],1,1,sigmoid,n=0.0)
         ]
     )
